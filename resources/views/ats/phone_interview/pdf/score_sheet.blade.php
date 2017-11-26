b942068763ed645eeb83e26bc0ffae90dbdfd27c
jubaedprince <jubaedprince@hotmail.com>
2017-11-26 16:30:42 +0600

    updates

--- resources/views/ats/phone_interview/pdf/score_sheet.blade.php
+++ resources/views/ats/phone_interview/pdf/score_sheet.blade.php
@@ -4,7 +4,6 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     </head>
     <body>
-
         @foreach($students as $student)
             <div>
                 <table border="1" style="border-collapse: collapse;">
