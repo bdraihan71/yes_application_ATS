a1671b4cf59bf50378af02e27dbb5c0a1f4be511
jubaedprince <jubaedprince@hotmail.com>
2017-12-05 17:07:54 +0600

    4 stickers per student

--- resources/views/ats/eltis/pdf/sit-sticker.blade.php
+++ resources/views/ats/eltis/pdf/sit-sticker.blade.php
@@ -22,26 +22,33 @@
     @endphp
 
         @foreach($students as $student)
-
-            <div style="border:1px solid black; width: 350px; height: 90px; float: left">
-                <div style="float:left; padding: 2%">
-                    <img  width="70px" src="/images/ats/eltis_students/{{$student->applicant_id}}.jpg">
-                </div>
-                <div style="text-align:left; font-size: 80%; padding: 2%">
-                    <b></b>
-                    <b>{{$student->first_name}} {{$student->last_name}}</b><br>
-                    <b>Applicant ID: {{$student->applicant_id}}</b><br><br>
-                    {{$student->schoolName}}, {{$student->district}}
-                </div>
-            </div>
-
             @php
-                $count = $count + 1;
-                if($count == 22){
-                    echo('<div class="page-break"></div>');
-                    $count = 0;
-                }
+                $counter = 0;
             @endphp
+
+            @while($counter<4)
+                <div style="border:1px solid black; width: 350px; height: 90px; float: left">
+                    <div style="float:left; padding: 2%">
+                        <img  width="70px" src="/images/ats/eltis_students/{{$student->applicant_id}}.jpg">
+                    </div>
+                    <div style="text-align:left; font-size: 80%; padding: 2%">
+                        <b></b>
+                        <b>{{$student->first_name}} {{$student->last_name}}</b><br>
+                        <b>Applicant ID: {{$student->applicant_id}}</b><br><br>
+                        {{$student->schoolName}}, {{$student->district}}
+                    </div>
+                </div>
+
+                @php
+                    $count = $count + 1;
+                    $counter = $counter + 1;
+                    if($count == 22){
+                        echo('<div class="page-break"></div>');
+                        $count = 0;
+                    }
+                @endphp
+            @endwhile
+
         @endforeach
     </body>
 </html>
