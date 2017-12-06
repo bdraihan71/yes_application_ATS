0d68a0361e3fdf63458ca99275ee04375f33f544
jubaedprince <jubaedprince@hotmail.com>
2017-12-06 14:06:42 +0600

    ID design changed to fit the feedback by Moni bhai

--- resources/views/ats/eltis/pdf/idcard.blade.php
+++ resources/views/ats/eltis/pdf/idcard.blade.php
@@ -14,6 +14,22 @@
         .page-break {
             page-break-after: always;
         }
+        .logo {
+            padding-right: 10px;
+            padding-left: 10px;
+        }
+        h1, h2, h3, h4 {
+            padding: 0;
+            margin: 5px;
+        }
+
+        .vol-copy, .ans-copy {
+            border-bottom: 1px dotted black;
+        }
+        #app-id {
+            border-top: 3px solid red;
+            border-bottom: 3px solid red;
+        }
     </style>
     <body>
 
@@ -23,8 +39,9 @@
 
         @foreach($students as $student)
 
-            <div style="border:1px solid black; height: 700px; width: 320px; float: left;">
-                <div>
+            <div style="border:1px solid black; height: 718px; width: 320px; float: left;">
+                <div class="vol-copy">
+                    <br>
                     <div style="float:left; padding: 2%">
                         <img  width="50px" src="/images/ats/eltis_students/{{$student->applicant_id}}.jpg">
                     </div>
@@ -32,11 +49,13 @@
                         <b>VOLUNTEER COPY: Give this part to volunteer before entering test-hall</b><br>
                         {{$student->first_name}} {{$student->last_name}}<br>
                         Applicant ID: {{$student->applicant_id}}
-                        <hr>
+                        <br><br>
+
                     </div>
                 </div>
 
-                <div>
+                <div class="ans-copy">
+                    <br>
                     <div style="float:left; padding: 2%">
                         <img  width="50px" src="/images/ats/eltis_students/{{$student->applicant_id}}.jpg">
                     </div>
@@ -44,27 +63,29 @@
                         <b>ANSWER SHEET COPY: Attach this part to your ELTiS Answer Script</b><br>
                         {{$student->first_name}} {{$student->last_name}}<br>
                         Applicant ID: {{$student->applicant_id}}
-                        <hr>
+                        <br><br>
                     </div>
                 </div>
 
 
                 <div>
-                    <img src="/images/ats/dos_logo.png" width="75px">
-                    <img src="/images/ats/yes_logo.png" width="75px">
-                    <img src="/images/ats/iearnbd_logo.png" width="75px">
+                    <br>
+                    <img src="/images/ats/dos_logo.png" class="logo" width="75px">
+                    <img src="/images/ats/yes_logo.png" class="logo" width="75px">
+                    <img src="/images/ats/iearnbd_logo.png" class="logo" width="75px">
                     <h4>International Education and Resource Network</h4>
-                    <h3>Kennedy-Lugar Youth Exchange and Study Program</h3>
+                    <h3>Kennedy-Lugar Youth Exchange</h3>
+                    <h3>and Study Program</h3>
                     <h4>Academic Year 2018-2019</h4>
                 </div>
                 <div>
-                    <h1>ELTiS Test Round</h1>
+                    <h1 style="color: darkblue;">ELTiS Test Round</h1>
                     <h3>Friday, 08 December 2017</h3>
                     <img width="100px" src="/images/ats/eltis_students/{{$student->applicant_id}}.jpg"><br>
                     <b>{{$student->first_name}} {{$student->last_name}}</b><br>
-                    {{$student->schoolName}}<br>
+                    {{$student->schoolName}},
                     {{$student->district}}
-                    <p>Applicant ID: {{$student->applicant_id}}</p>
+                    <p id="app-id">Applicant ID: {{$student->applicant_id}}</p>
                 </div>
             </div>
 
