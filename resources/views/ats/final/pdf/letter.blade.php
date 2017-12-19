8f54b0aa8b13ef3111abd9b7e834a9facbe2461d
jubaedprince <jubaedprince@hotmail.com>
2017-12-19 16:11:21 +0600

    letter styled

--- resources/views/ats/final/pdf/letter.blade.php
+++ resources/views/ats/final/pdf/letter.blade.php
@@ -50,19 +50,19 @@
                                 Dear <b>{{$student->first_name}} {{$student->last_name}},</b>
                             </p>
                             <p>
-                                Congratulations! You have qualified in ELTiS and In-Class Essay of the Kennedy Lugar Youth Exchange & Study Program for Academic Year 2018-2019. We are delighted to invite you to participate in Final Application & Interview Round of the recruitment.
+                                Congratulations! You have qualified in <b>ELTiS and In-Class Essay</b> of the <b>Kennedy Lugar Youth Exchange & Study Program</b> for <b>Academic Year 2018-2019</b>. We are delighted to invite you to participate in <b>Final Application & Interview Round</b> of the recruitment.
                             </p>
 
                             <p>
-                                Your YES program Final Application has been received and you have been to participate in the next phase, which is the Group and Individual Interview. The interview will be held at the American Center, Dhaka; Progati Sharoni, Plot 1, J-Block, Dhaka 1212.
+                                Your YES program Final Application has been received and you have been to participate in the next phase, which is the <b>Group and Individual Interview</b>. The interview will be held at the <b>American Center</b>, Dhaka; Progati Sharoni, Plot 1, J-Block, Dhaka 1212.
                             </p>
 
                             <p>
-                                You are requested to be present at the venue sharp at the time allocated for you. It is absolutely mandatory for one of your parents to be present with you on the day of your interview, please ensure the parent whose name you have provided, come with you to the interview. Parents and applicants are required to carry a photo ID (e.g. passport/ national ID/ Office ID or any ID that has photo and name) with them on the day of the interview. For security reasons, only the designated person mentioned in the online form will be allowed to enter.
+                                You are requested to be present at the venue sharp at the time allocated for you. It is <b>absolutely mandatory</b> for one of your parents to be present with you on the day of your interview, please ensure the parent whose name you have provided, come with you to the interview. Parents and applicants are <b>required</b> to carry a photo ID (e.g. passport/ national ID/ Office ID or any ID that has photo and name) with them on the day of the interview. For security reasons, only the designated person mentioned in the online form will be allowed to enter.
                             </p>
 
                             <p>
-                                You must also bring all your original supporting documents (Birth certificate, Passport, Transcripts, Vaccination Records and Achievement Certificate) that you have submitted along with your Final Application for verification.
+                                You <b>must</b> also bring all your <b>original supporting documents</b> (Birth certificate, Passport, Transcripts, Vaccination Records and Achievement Certificate) that you have submitted along with your Final Application for verification.
                             </p>
 
                             <p>
@@ -73,7 +73,7 @@
                             </p>
 
                             <p>
-                                Your parent, {{$student->parent_for_interview}} will be attending the parent session. Parent will be required to display national id in the gate of the American Centre, Dhaka.
+                                <b>Your parent, {{$student->parent_for_interview}} will be attending the parent session. Parent will be required to display national id in the gate of the American Centre, Dhaka.</b>
                             </p>
 
 
@@ -98,7 +98,7 @@
                     Address of American Center, Dhaka<br>
                     Progati Sharoni, Plot 1,<br>
                     J-Block, Dhaka 1212<br>
-                    <img width="300px" src="/images/ats/am_center_map.png">
+                    <img width="350px" src="/images/ats/am_center_map.png">
                 </div>
             </div>
 
