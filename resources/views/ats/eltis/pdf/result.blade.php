5f38fc0bb6fbd6b373f26aa703e44fa88f28f1d8
Gulshan Jubaed Prince <prince@techynaf.com>
2019-12-17 06:50:46 +0600

    Fix result of eltis test

--- resources/views/ats/eltis/pdf/result.blade.php
+++ resources/views/ats/eltis/pdf/result.blade.php
@@ -6,20 +6,28 @@
     <body style="margin-top: 1.65in; margin-bottom: .6in">
 
         <table border="1" style="border-collapse: collapse;">
-            {{--<tr>--}}
-                {{--<td>ID</td>--}}
-                {{--<td>Name</td>--}}
-                {{--<td>School</td>--}}
-                {{--<td>Status</td>--}}
-            {{--</tr>--}}
-
+            <tr>
+                <td>Serial</td>
+                <td>Name</td>
+                <td>School</td>
+                <!-- <td>Status</td> -->
+            </tr>
+            @php
+                $count = 1;
+            @endphp
             @foreach($students as $student)
+                @if($student->stage == 4)
+                
                 <tr>
-                    <td>{{$student->applicant_id}}</td>
+                    <td>{{$count}}</td>
                     <td>{{$student->first_name}} {!! !empty($student->middle_name)?$student->middle_name:'' !!} {{$student->last_name}}</td>
                     <td>{{$student->schoolName}}</td>
-                    <td>{!! $student->stage == 4 ? 'Successful' : 'Unsuccessful' !!}</td>
+                    <!-- <td>{!! $student->stage == 4 ? 'Successful' : 'Unsuccessful' !!}</td> -->
+                    @php
+                        $count = $count + 1;
+                    @endphp
                 </tr>
+                @endif
             @endforeach
         </table>
     </body>
