dc9edb56709ca3891586733dd38ed215fcb65e85
bdraihan71 <raihan@techynaf.com>
2019-12-28 14:39:40 +0600

    Fix registration

--- resources/views/ats/final-interview/pdf/registration.blade.php
+++ resources/views/ats/final-interview/pdf/registration.blade.php
@@ -58,14 +58,14 @@
             <h3>Kennedy Lugar Youth Exchange and Study (YES) Program</h3>
             <h4>Academic Year 2020-21</h4>
             <h3>Registration Sheet: Final Application & Interview Round</h3>
-            <p>Date: 7-9 January 2018</p>
+            <p>Date:  5,6 & 7 January 2020</p>
         </div>
         <img  style="width:100px; background-color: blue ; float: left" src="/images/ats/iearnbd_logo.png">
     </div>
 
 
     <div style="clear: left">
-        <div class="col" style="width: 9%;">Applicant ID</div>
+        <div class="col" style="width: 9%;">Serial</div>
         <div class="col" style="width: 9%; ">Name</div>
         <div class="col" style="width: 9%; ">Image</div>
         <div class="col" style="width: 18%; ">School, District</div>
@@ -76,7 +76,7 @@
 
     @foreach($students as $student)
         <div>
-            <div class="col" style="width: 9%;">{{$student->applicant_id}}</div>
+            <div class="col" style="width: 9%;">{{$student->final_id}}</div>
             <div class="col" style="width: 9%; ">{{$student->first_name}} {{$student->last_name}}</div>
             <div class="col" style="width: 9%; text-align: center;">
                 <img  width="64px" src="{{$student->photo}}">
