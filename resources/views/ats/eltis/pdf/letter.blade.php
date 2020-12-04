a3c13215caf7a8fe6bb976879fcbe00354fa441d
Raihan Farhad <raihan@techynaf.com>
2020-12-05 01:14:34 +0600

    Fix letter

--- resources/views/ats/eltis/pdf/letter.blade.php
+++ resources/views/ats/eltis/pdf/letter.blade.php
@@ -17,10 +17,10 @@
             <div style="margin-top: 1in">
                 <div>
                     <div style="width: 400px;  display: inline-table">
-                        <p>Ref iEARN-BD/YES-18/ELTiS/2019-06</p><br>
+                        <p>Ref iEARN-BD/YES-19/ELTiS/2020-04</p><br>
                     </div>
-                    <div align="right" style="width: 200px;  display: inline-table" >
-                        <p><b>December 8, 2019</b></p>
+                    <div style="width: 200px;  display: inline-table ; margin-left: -100px" >
+                        <p><b>December 5, 2020</b></p>
                     </div>
                 </div>
                 <div>
@@ -44,46 +44,50 @@
                     <div>
                         <div>
                             <p>
-                                <b>Sub: Invitation to appear in English Language Test for International Students (ELTiS) and In-Class Essay round of K-L YES Program for the academic year 2021-2021.</b>
+                                <b>Sub: Invitation to appear in English Language Test for International Students (ELTiS) and In-Class Essay round of K-L YES Program for the academic year 2021-2022.</b>
                             </p>
                             <p>
                                 Dear <b>{{$student->first_name}} {{$student->last_name}},</b>
                             </p>
                             <p>
-                                Congratulations! You have successfully passed the <b>Phone Interview</b> round of the recruitment process of Kennedy-Lugar Youth Exchange & Study Program for the academic year 2021-22. We are delighted to invite you to participate in the <b>English Language Test for International Students (ELTiS) and In-Class Essay</b> round of the recruitment process.
+                                Congratulations! You have successfully passed the Phone Interview round in the qualification process of the Kennedy-Lugar Youth Exchange & Study Program of the 2021-2022 academic year. We are delighted to invite you to participate in the next round of the qualification process – the English Language Test for International Students (ELTiS) and In-Class Essay round.
                             </p>
                             <p>
-                                The ELTiS & In-Class Essay Examination will be held on <b>Friday, December 13, 2019</b>, in an examination hall of <b>FutureEd</b>, Ahmed Tower (6th Floor), 28 & 30 Kamal Ataturk Avenue, Banani, Dhaka-1213. If you need any assistance locating the venue, please feel free to contact us at +8801313946086.
+                                For more information on the ELTiS test, please go to this website:<u> www.eltistest.com/students</u>
                             </p>
 
                             <p>
-                                The following is the time slot of your exam. Please follow the list on our website and refer to the serial on this list to know which slot you are in. 
+                                You have been emailed about the time date and venue address of the ELTiS and In Class Essay exam.
                             </p>
                             <p>
-                                <b>
-                                The reporting time for SLOT 1: Serial 1 -100 is 08:15am. Exam time- 09:00am - 11:00am <br>
-                                The reporting time for SLOT 2: Serial 101- 157 is 10:45am. Exam time- 11:30am - 01:30pm
-                                </b>
+                                You <b>MUST</b> report at the examination venue by the given time and stand in line in front of the registration desk.<b>Failure to report at the registration desk within the reporting time will automatically disqualify you from the ELTiS and In-Class Essay round.</b> 
                             </p>
                             <p>
-                                You MUST report at the examination venue by the given time and wait downstairs until a volunteer escorts you to the hall in a group. Please ensure your parents wait downstairs and do not come up with you - it will be disruptive to other exam candidates. <b> Failure to show up within reporting time will automatically disqualify you from the process.</b>
+                                After your registration is complete, and your body temperature is tested, you will climb up the stairs of the building to the 3rd floor, and enter the examination venue.
                             </p>
                             <p>
-                                You are not allowed to bring your cell phones or any electronic devices with you to the examination hall. You will be provided with stationery at the examination hall.
+                                Due to the ongoing pandemic only the student will wait in line in front of the registration desk.<b> The parents are NOT allowed to climb up the stairs and go to the exam venue during the examination.</b> After the examination is over, the student <b>MUST</b> leave the exam venue, climb down the stairs, and leave the iEARN-BD office building.
+                            </p>
+                            <p>
+                                You are not allowed to bring your cell phones or any other electronic devices with you to the examination venue. You will be provided with the necessary stationery for the exam at the venue.
                             </p>
                             <p>
                                 No T.A./D.A. will be given for appearing in the ELTiS and In-Class Essay Round. 
                             </p>
                             <p>
-                                <b>Please bring this letter with you on the day of the ELTiS exam.</b>
+                                <p>You must also bring your printed preliminary application form with the signature page, and the attested photocopies of the supporting documents you submitted during application, such as transcripts and birth certificate. </p> The supporting documents must be attested by either a gazetted government official or by your school/college institutions’ headmaster/headmistress/principal.
+                            </p>
+
+                            <p>
+                                <b>Disclaimer: </b>Do not provide any ORIGINAL transcript and birth certificate. Only provide attested photocopies. We will not take responsibility for the safekeeping of your original document(s).
+                            </p>
+
+                            <p>
+                                We hope to find you in the best health for this round.
                             </p>
                             <p>
-                                <b>You must also bring the original copy of your printed preliminary application form- with the original signature page and attested copies of the documents you provided with the preliminary application form.</b>(keep a copy for yourself as well).
+                                Best of luck!
                             </p>
-                            <p>
-                                <i>Disclaimer: Do not provide any ORIGINAL transcript, birth certificate </i>- only provide attested copies. We will not take responsibility for the safekeeping of your original document(s). 
-                            </p>
-                            <p>For any queries, please email us at yesinfo@iearnbd.org.</p>
                         </div>
                     </div>
                 </div>
@@ -96,17 +100,17 @@
                     </p>
                     <img src="/images/ats/wasi.png"><br>
                     {{--<br><br>--}}
-                    Wasi Mahmud Moni <br>
+                    Nawab Wasi Mahmud<br>
                     Director <br>
-                    iEARN-BD and K-L YES Program <br>
+                    iEARN-BD and K-L YES Program 
                     in Bangladesh
                 </div>
-                <div style="width: 350px; display: inline-table" >
+                {{-- <div style="width: 350px; display: inline-table" >
                     Address of Exam Venue:<br>
                     FutureED | (6th Floor) Ahmed Tower,<br>
                     28 & 30 Kamal Ataturk Avenue, Banani, Dhaka 1213<br>
                     <img width="250px" src="/images/ats/map.png">
-                </div>
+                </div> --}}
             </div>
 
             <div class="page-break"></div>
